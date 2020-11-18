52b2ce2f50649eb23b580f5a0bed201dd95873b5
lrd003 <leo.gonsa003@gmail.com>
2020-11-18 15:05:13 -0300

    Atualizando o projeto

--- src/main/java/aep_poo/aep/AppHome.java
+++ src/main/java/aep_poo/aep/AppHome.java
@@ -3,19 +3,26 @@ package aep_poo.aep;
 public class AppHome {
     public static void main(String[] args) {
         
+        //----------------------Adicionando Informações no Perfil-------------------------------------
         Perfil exemplo = new Perfil("Leonardo", "leonardo@gmail.com", "1235223525", "99999999","bababab");
         Perfil exemplo2 = new Perfil("Rodrigues", "Rodrigues@gmail.com", "012355555", "10000000","senha");
+        //----------------------Adicionando Informações no Perfil-------------------------------------
 
+        //---------------------------------Adicionando Informações de Endereço----------------------------
         Endereco enderecoVitima = new Endereco("Rua leopoldino sei la do que", "1007", "Cianorte", "Zona - 1 ", "Brasil", "PR");
-        Endereco enderecoFamiliar = new Endereco("Barão da bisadinha", "200", "Maringá", "Zona - 5 ", "Brasil", "PR");
+        Endereco enderecoFamiliarVitima = new Endereco("Barão da bisadinha", "200", "Maringá", "Zona - 5 ", "Brasil", "PR");
         Endereco endereçoRodrigues = new Endereco("Algum lugar de Maringá", "222", "Maringá", "Zona Leste", "Brasil", "PR");
+        //---------------------------------Adicionando Informações de Endereço----------------------------
 
+        //Adicionando Endereço no Perfil do Usuário--------------------------------
         exemplo.addEndereço(enderecoVitima);
-        exemplo.addEndereço(enderecoFamiliar);
-       
-        exemplo.setEndereçoPrincipal(enderecoVitima);
-
+        exemplo.addEndereço(enderecoFamiliarVitima);
         exemplo2.addEndereço(endereçoRodrigues);
+        //Adicionando Endereço no Perfil do Usuário--------------------------------
+ 
+        //Salvando Endereço no Usuário---------------------------------------------
+        exemplo.setEndereçoPrincipal(enderecoVitima);
+        //Salvando Endereço no Usuário---------------------------------------------
 
         System.out.println(exemplo);
         System.out.println(exemplo2);
